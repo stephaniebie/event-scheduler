d1131707ee38e24ea49ab64a1b992a85c412f83b
dnyanadab <75079786+dnyanadab@users.noreply.github.com>
2025-10-07 17:08:54 -0600

    search.py

--- src/scheduler/search.py
+++ src/scheduler/search.py
@@ -6,10 +6,39 @@ from scheduler.eventlist import EventList, LinkedEventList
 
 def linear_search(data, target):
     raise NotImplementedError
+    n = len(data)
+    
+    # Iterate over the array in order to
+    # find the key x
+    for i in range(0, n):
+        if (arr[i] == target):
+            return i
+    return -1
 
 
 def binary_search(data, target):
     raise NotImplementedError
+    low = 0
+    high = len(data) - 1
+    while low <= high:
+
+        mid = low + (high - low) // 2
+
+        # Check if x is present at mid
+        if arr[mid] == target:
+            return mid
+
+        # If x is greater, ignore left half
+        elif arr[mid] < target:
+            low = mid + 1
+
+        # If x is smaller, ignore right half
+        else:
+            high = mid - 1
+
+    # If we reach here, then the element
+    # was not present
+    return -1
 
 
 class SearchAlgorithm(Enum):
